50af6ece3bb5cdf956d048ef1f2e1e2ed656d922
sebas <barmoraseb@gmail.com>
2026-09-18 20:47:00 -0600

    initial commit

---
+++ DominoAllFives.Client/DominoAllFives.Client.WPF/Services/Placeholder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DominoAllFives.Client.WPF.Services
+{
+    internal class Placeholder
+    {
+    }
+}
---
+++ DominoAllFives.Client/DominoAllFives.Client.WPF/ViewModels/Placeholder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DominoAllFives.Client.WPF.ViewModels
+{
+    internal class Placeholder
+    {
+    }
+}
