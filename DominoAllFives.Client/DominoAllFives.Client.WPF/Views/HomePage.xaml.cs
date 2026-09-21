8f0256ddcf3f3ba68029bcc906725f524fa64ef3
sebas <barmoraseb@gmail.com>
2026-09-20 19:20:29 -0600

    feat(home): created HomePage.xaml and added global style for ComboBox

---
+++ DominoAllFives.Client/DominoAllFives.Client.WPF/Views/HomePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DominoAllFives.Client.WPF.Views
+{
+    /// <summary>
+    /// Lógica de interacción para HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Window
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButtonClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
